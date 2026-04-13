4d4eea35bf6c6117a8922962af46de665dc94d8a
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2026-04-13 18:00:51 +0200

    Reimplemented as discriminated union

--- src/main/java/dev/mhh/results/Result.java
+++ src/main/java/dev/mhh/results/Result.java
@@ -1,64 +1,120 @@
 package dev.mhh.results;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-public class Result<T, E> {
-    private final T value;
-    private final E error;
-
-    private Result(T value, E error) {
-        this.value = value;
-        this.error = error;
-    }
-
-    public static <T, E> Result<T, E> of(T value) {
-        return new Result<>(value, null);
-    }
-
-    public static <T, E> Result<T, E> error(E error) {
-        return new Result<>(null, error);
-    }
-
-    public boolean isError() {
-        return error != null;
-    }
-
-    public boolean isSuccess() {
-        return !isError();
-    }
-
-    @Override
-    public String toString() {
-        if (isError()) {
-            return "Result error (" + error + ")";
+public sealed interface Result<T, E> {
+    record Ok<T, E>(T ok) implements Result<T, E>{
+        @Override
+        public String toString() {
+            return "Ok[" + ok + ']';
         }
-        return "Result success (" + value + ")";
+
+        public Ok {
+            Objects.requireNonNull(ok);
+        }
+
+        @Override
+        public Optional<T> value() {
+            return Optional.of(ok);
+        }
+
+        @Override
+        public Optional<E> error() {
+            return Optional.empty();
+        }
+
+        @Override
+        public boolean isOk() {
+            return true;
+        }
+
+        @Override
+        public boolean isError() {
+            return false;
+        }
+
+        @Override
+        public <R> Result<R, E> map(Function<T, R> mapper) {
+            final var value = mapper.apply(ok);
+            return Result.ok(value);
+        }
+
+        @Override
+        public <R> Result<R, E> flatMap(Function<T, Result<R, E>> mapper) {
+            return mapper.apply(ok);
+        }
+
+        @Override
+        public Result<T, E> consume(Consumer<T> consumer) {
+            consumer.accept(ok);
+            return this;
+        }
     }
 
-    public Optional<T> value() {
-        return Optional.ofNullable(value);
+    record Err<T, E>(E err) implements Result<T, E>{
+        public Err {
+            Objects.requireNonNull(err);
+        }
+
+        @Override
+        public Optional<T> value() {
+            return Optional.empty();
+        }
+
+        @Override
+        public Optional<E> error() {
+            return Optional.ofNullable(err);
+        }
+
+        @Override
+        public String toString() {
+            return "Err[" + err + ']';
+        }
+
+        @Override
+        public boolean isOk() {
+            return false;
+        }
+
+        @Override
+        public boolean isError() {
+            return true;
+        }
+
+        @Override
+        public <R> Result<R, E> map(Function<T, R> mapper) {
+            return Result.err(err);
+        }
+
+        @Override
+        public <R> Result<R, E> flatMap(Function<T, Result<R, E>> mapper) {
+            return Result.err(err);
+        }
+
+        @Override
+        public Result<T, E> consume(Consumer<T> consumer) {
+            return this;
+        }
     }
 
-    public Optional<E> error() {
-        return Optional.ofNullable(error);
+    static <T, E> Result<T, E> ok(T value) {
+        return new Ok<>(value);
     }
 
-    public <R> Result<R, E> map(Function<T, R> mapper) {
-        return isSuccess()
-                ? of(mapper.apply(value))
-                : error(error);
+    static <T, E> Result<T, E> err(E value) {
+        return new Err<>(value);
     }
 
-    public <R> Result<R, E> flatMap(Function<T, Result<R, E>> mapper) {
-        return isSuccess()
-                ? mapper.apply(value)
-                : error(error);
-    }
+    Optional<T> value();
+    Optional<E> error();
 
-    public Result<T, E> consume(Consumer<T> consumer) {
-        if (isSuccess()) consumer.accept(value);
-        return this;
-    }
+    boolean isOk();
+    boolean isError();
+
+    <R> Result<R, E> map(Function<T, R> mapper);
+    <R> Result<R, E> flatMap(Function<T, Result<R, E>> mapper);
+    Result<T, E> consume(Consumer<T> consumer);
 }
--- src/test/java/dev/mhh/results/ResultTest.java
+++ src/test/java/dev/mhh/results/ResultTest.java
@@ -8,21 +8,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResultTest {
-    Result<Long, Long> success10 = Result.of(10L);
-    Result<Long, Long> error10 = Result.error(10L);
+    Result<Long, Long> ok10 = Result.ok(10L);
+    Result<Long, Long> error10 = Result.err(10L);
 
     @Test
     void of() {
-        final var success = Result.of(10L);
+        final var success = Result.ok(10L);
 
-        assertTrue(success.isSuccess());
+        assertTrue(success.isOk());
         assertEquals(Optional.of(10L), success.value());
         assertEquals(Optional.empty(), success.error());
     }
 
     @Test
     void error() {
-        final var error = Result.error(10L);
+        final var error = Result.err(10L);
 
         assertTrue(error.isError());
         assertEquals(Optional.of(10L), error.error());
@@ -31,21 +31,21 @@ class ResultTest {
 
     @Test
     void toStringTest() {
-        assertEquals("Result success (10)", success10.toString());
-        assertEquals("Result error (10)", error10.toString());
+        assertEquals("Ok[10]", ok10.toString());
+        assertEquals("Err[10]", error10.toString());
     }
 
     @Test
     void mapWhenSuccess() {
-        final var success20 = success10.map(x -> x * 2);
+        final var ok20 = ok10.map(x -> x * 2);
 
-        assertTrue(success20.isSuccess());
-        assertEquals(Optional.of(20L), success20.value());
-        assertEquals(Optional.empty(), success20.error());
+        assertTrue(ok20.isOk());
+        assertEquals(Optional.of(20L), ok20.value());
+        assertEquals(Optional.empty(), ok20.error());
 
-        assertTrue(success10.isSuccess());
-        assertEquals(Optional.of(10L), success10.value());
-        assertEquals(Optional.empty(), success10.error());
+        assertTrue(ok10.isOk());
+        assertEquals(Optional.of(10L), ok10.value());
+        assertEquals(Optional.empty(), ok10.error());
 
     }
 
@@ -64,21 +64,21 @@ class ResultTest {
 
     @Test
     void flatMapWhenSuccess() {
-        final var success20 = success10.flatMap(x -> Result.of(x * 2));
+        final var ok20 = ok10.flatMap(x -> Result.ok(x * 2));
 
-        assertTrue(success20.isSuccess());
-        assertEquals(Optional.of(20L), success20.value());
-        assertEquals(Optional.empty(), success20.error());
+        assertTrue(ok20.isOk());
+        assertEquals(Optional.of(20L), ok20.value());
+        assertEquals(Optional.empty(), ok20.error());
 
-        assertTrue(success10.isSuccess());
-        assertEquals(Optional.of(10L), success10.value());
-        assertEquals(Optional.empty(), success10.error());
+        assertTrue(ok10.isOk());
+        assertEquals(Optional.of(10L), ok10.value());
+        assertEquals(Optional.empty(), ok10.error());
 
     }
 
     @Test
     void flatMapWhenError() {
-        final var error20 = error10.flatMap(x -> Result.of(x * 2));
+        final var error20 = error10.flatMap(x -> Result.ok(x * 2));
 
         assertTrue(error20.isError());
         assertEquals(Optional.of(10L), error20.error());
@@ -92,13 +92,13 @@ class ResultTest {
     @Test
     void consumeWhenSuccess() {
         final var consumed = new AtomicBoolean(false);
-        final var consumedResult = success10.consume(_ -> consumed.set(true));
+        final var consumedResult = ok10.consume(_ -> consumed.set(true));
 
-        assertTrue(success10.isSuccess());
-        assertEquals(Optional.of(10L), success10.value());
-        assertEquals(Optional.empty(), success10.error());
+        assertTrue(ok10.isOk());
+        assertEquals(Optional.of(10L), ok10.value());
+        assertEquals(Optional.empty(), ok10.error());
 
-        assertTrue(consumedResult.isSuccess());
+        assertTrue(consumedResult.isOk());
         assertEquals(Optional.of(10L), consumedResult.value());
         assertEquals(Optional.empty(), consumedResult.error());
 
